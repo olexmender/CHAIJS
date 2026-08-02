a6b75aa090d961cf8b42a268888aaf2d9bfae8fc
olexmender <fives9833@gmail.com>
2026-08-02 20:06:45 +0000

    array 2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,19 @@
+const marvel_heroes=["thor","ironman","spiderman"];
+const dc_heroes=["superman","flash","batman"];
+
+// marvel_heroes.push(dc_heroes);
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+// let allHeroes=marvel_heroes.concat(dc_heroes);
+// console.log(allHeroes);
+
+// const all_new_heroes=[...marvel_heroes,...dc_heroes]; //spread operator
+// console.log(all_new_heroes);
+
+// const another_array=[1,2,3,4,[5,6,7],[8,9,10,[11,12,13,14]]];
+// const real_another_array=another_array.flat(Infinity);
+// console.log(real_another_array);
+
+console.log(Array.isArray("sameer"));
+console.log(Array.from("sameer"));
