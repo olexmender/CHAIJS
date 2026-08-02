7c0d90db808116653e795869072175dd68c96edd
olexmender <fives9833@gmail.com>
2026-08-02 19:34:09 +0000

    arrays part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,30 @@
+let myArr=[1,2,3,4,5,6];
+// let myHeroes=["spiderman","shaktiman","hulkubaba","antman"];
+// const myArr2=new Array(9,5,2,1,15,5);
+// console.log(myArr);
+// console.log(myHeroes);
+// console.log(myArr2);
+
+// myArr.push(7);
+// console.log(myArr);
+// myArr.pop();
+// // console.log(myArr);
+
+// myArr.unshift(9);
+// myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(5));
+
+// const newArr=myArr.join();
+// console.log(myArr);
+// console.log(typeof(newArr));
+
+// console.log("A",myArr);
+// const myn1=myArr.slice(0,3);
+// console.log(myn1);
+// const myn2=myArr.splice(0,3);
+// console.log(myn2);
+
+let newArr=myArr.splice(1,4);
+console.log(newArr);
